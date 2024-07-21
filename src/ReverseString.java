ee1929dfd2460dba47df9ddc33dbe99ecbe1d2a9
Nilson Santiago <nilson.santiago19@gmail.com>
2024-07-21 17:17:01 -0300

    Reverse String challenge met and change in readme file

---
+++ src/ReverseString.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a text: ");
+        String text = sc.nextLine();
+        String newText = "";
+
+        for(int i = text.length() - 1; i >= 0; i--) {
+            newText += text.charAt(i);
+        }
+
+        System.out.println(newText);
+    }
+}
